d745186bc14079d51117c173527c305b6da6b9e2
MarlonMoreno <marlonmoreno894@gmail.com>
2023-10-01 22:04:02 -0500

    Made React base for project

---
+++ pages/projects/the-playlister.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function ThePlaylister() {
+  return (
+    <>
+      <h1>The Playlister</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
